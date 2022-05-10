d777e0c8f41f70bb148db89e9ded8a23caa08f2a
minarypenguin <alexfinhart@gmail.com>
2022-05-10 14:23:37 +0800

    Revert "Messaging: Show quick reply choices only on wearable devices"
    This reverts commit 1b6b3b244fc3a05327671fc19e1f5bbea95d3aa1.
    
    Change-Id: I4c2ae559433ea527e360fdc4c693821c8d776015

--- src/com/android/messaging/datamodel/BugleNotifications.java
+++ src/com/android/messaging/datamodel/BugleNotifications.java
@@ -788,7 +788,7 @@ public class BugleNotifications {
             maybeAddWearableConversationLog(wearableExtender,
                     (MultiMessageNotificationState) notificationState);
             addDownloadMmsAction(notifBuilder, wearableExtender, notificationState);
-            addWearableVoiceReplyAction(notifBuilder, wearableExtender, notificationState);
+            addWearableVoiceReplyAction(wearableExtender, notificationState);
         }
 
         // Apply the wearable options and build & post the notification
@@ -830,7 +830,7 @@ public class BugleNotifications {
         }
     }
 
-    private static void addWearableVoiceReplyAction(final NotificationCompat.Builder notifBuilder,
+    private static void addWearableVoiceReplyAction(
             final WearableExtender wearableExtender, final NotificationState notificationState) {
         if (!(notificationState instanceof MultiMessageNotificationState)) {
             return;
@@ -860,20 +860,14 @@ public class BugleNotifications {
         final NotificationCompat.Action.Builder actionBuilder =
                 new NotificationCompat.Action.Builder(R.drawable.ic_wear_reply,
                         context.getString(replyLabelRes), replyPendingIntent);
-        final RemoteInput.Builder remoteInputBuilder = new RemoteInput.Builder(Intent.EXTRA_TEXT);
-        remoteInputBuilder.setLabel(context.getString(R.string.notification_reply_prompt));
-        actionBuilder.addRemoteInput(remoteInputBuilder.build());
-        notifBuilder.addAction(actionBuilder.build());
-
-        // Support the action on a wearable device
-        final NotificationCompat.Action.Builder wearActionBuilder =
-                new NotificationCompat.Action.Builder(R.drawable.ic_wear_reply,
-                        context.getString(replyLabelRes), replyPendingIntent);
         final String[] choices = context.getResources().getStringArray(
                 R.array.notification_reply_choices);
-        remoteInputBuilder.setChoices(choices);
-        wearActionBuilder.addRemoteInput(remoteInputBuilder.build());
-        wearableExtender.addAction(wearActionBuilder.build());
+        final RemoteInput remoteInput = new RemoteInput.Builder(Intent.EXTRA_TEXT).setLabel(
+                context.getString(R.string.notification_reply_prompt)).
+                setChoices(choices)
+                .build();
+        actionBuilder.addRemoteInput(remoteInput);
+        wearableExtender.addAction(actionBuilder.build());
     }
 
     private static void addDownloadMmsAction(final NotificationCompat.Builder notifBuilder,
